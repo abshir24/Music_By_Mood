61d6a3b6b72c773b81054ae3e568023c36dd5503
Abshir <abshirm@live.com>
2018-01-22 13:12:16 -0800

    Integrated angular as client side

---
+++ Music_By_Mood/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Music_By_Mood project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Music_By_Mood.settings")
+
+application = get_wsgi_application()
